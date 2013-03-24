9b8a97ee3eccf449c06a6fea3ef86ff252fcb5f5
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-24 23:56:47 +0000

    Hooked SignInPage's form to its model.

--- src/main/java/com/friendfeed/web/panel/SignInPanel.java
+++ src/main/java/com/friendfeed/web/panel/SignInPanel.java
@@ -1,6 +1,15 @@
 package com.friendfeed.web.panel;
 
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.value.ValueMap;
+
+import com.friendfeed.core.domain.User;
+import com.friendfeed.web.FriendFeedApplication;
+import com.friendfeed.web.pages.FriendFeedHome;
 
 public class SignInPanel extends Panel {
 
@@ -8,6 +17,41 @@ public class SignInPanel extends Panel {
 
     public SignInPanel(String id) {
         super(id);
+
+        add(new SignInForm("signInForm"));
+    }
+
+    public final class SignInForm extends Form<Void> {
+
+        private static final long serialVersionUID = 7634349439380603856L;
+
+        private static final String USERNAME = "username";
+        private static final String PASSWORD = "password";
+
+        private final ValueMap properties = new ValueMap();
+
+        public SignInForm(final String id) {
+            super(id);
+
+            add(new TextField<String>(USERNAME, new PropertyModel<String>(properties, USERNAME)));
+            add(new PasswordTextField(PASSWORD, new PropertyModel<String>(properties, PASSWORD)));
+        }
+
+        @Override
+        public final void onSubmit() {
+            User user = new User();
+            user.setUsername(getUsername());
+            FriendFeedApplication.setCurrentUser(user);
+            setResponsePage(FriendFeedHome.class);
+        }
+
+        /**
+         * @return
+         */
+        private String getUsername() {
+            return properties.getString(USERNAME);
+        }
+
     }
 
 }
