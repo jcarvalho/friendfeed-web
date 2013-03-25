26308d69cf992651530404598925eae7f603f90c
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-25 00:24:29 +0000

    SignIn options are no longer required.

--- src/main/java/com/friendfeed/web/panel/SignInPanel.java
+++ src/main/java/com/friendfeed/web/panel/SignInPanel.java
@@ -6,12 +6,18 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.value.ValueMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.Atomic;
 
 import com.friendfeed.core.domain.User;
 import com.friendfeed.web.FriendFeedApplication;
 import com.friendfeed.web.pages.FriendFeedHome;
 
 public class SignInPanel extends Panel {
+
+    private static final Logger logger = LoggerFactory.getLogger(SignInPanel.class);
 
     private static final long serialVersionUID = -3480067697256907945L;
 
@@ -33,14 +39,18 @@ public class SignInPanel extends Panel {
         public SignInForm(final String id) {
             super(id);
 
-            add(new TextField<String>(USERNAME, new PropertyModel<String>(properties, USERNAME)));
-            add(new PasswordTextField(PASSWORD, new PropertyModel<String>(properties, PASSWORD)));
+            add(new TextField<String>(USERNAME, new PropertyModel<String>(properties, USERNAME)).setRequired(false));
+            add(new PasswordTextField(PASSWORD, new PropertyModel<String>(properties, PASSWORD)).setRequired(false));
         }
 
+        @Atomic
         @Override
         public final void onSubmit() {
+            logger.info("Got user '{}' with password '{}'", getUsername(), getPassword());
             User user = new User();
             user.setUsername(getUsername());
+            user.setEmail("blah@blah.com");
+            user.setPassword("blah");
             FriendFeedApplication.setCurrentUser(user);
             setResponsePage(FriendFeedHome.class);
         }
@@ -50,6 +60,10 @@ public class SignInPanel extends Panel {
          */
         private String getUsername() {
             return properties.getString(USERNAME);
+        }
+
+        private String getPassword() {
+            return properties.getString(PASSWORD);
         }
 
     }
