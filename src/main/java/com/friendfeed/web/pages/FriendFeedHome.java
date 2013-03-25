fe2e09d0d8ff14a17c86aca7b2c5a346738c43d7
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-25 00:24:08 +0000

    Home page now lists the application's users.

--- src/main/java/com/friendfeed/web/pages/FriendFeedHome.java
+++ src/main/java/com/friendfeed/web/pages/FriendFeedHome.java
@@ -1,14 +1,29 @@
 package com.friendfeed.web.pages;
 
-import org.apache.wicket.markup.html.basic.Label;
+import java.util.ArrayList;
+import java.util.List;
 
-import pt.ist.fenixframework.FenixFramework;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+
+import com.friendfeed.core.domain.FriendFeed;
+import com.friendfeed.core.domain.User;
 
 public class FriendFeedHome extends FriendFeedPage {
 
     private static final long serialVersionUID = 5966860435038911759L;
 
     public FriendFeedHome() {
-        add(new Label("message", FenixFramework.getDomainRoot().toString()));
+        add(new Label("message", FriendFeed.getInstance().toString()));
+        List<User> users = new ArrayList<>(FriendFeed.getInstance().getUsers());
+        add(new ListView<User>("users", users) {
+            private static final long serialVersionUID = 7041497838941274242L;
+
+            @Override
+            protected void populateItem(ListItem<User> item) {
+                item.add(new Label("label", item.getModel().getObject().getUsername()));
+            }
+        });
     }
 }
