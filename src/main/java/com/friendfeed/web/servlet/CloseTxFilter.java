34bb9736b12f7db382eb4651c871f8f2c4e9b3b0
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-26 18:29:05 +0000

    Migrate to Servlet 3.0

--- src/main/java/com/friendfeed/web/servlet/CloseTxFilter.java
+++ src/main/java/com/friendfeed/web/servlet/CloseTxFilter.java
@@ -8,19 +8,27 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
 
+@WebFilter("/*")
 public class CloseTxFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CloseTxFilter.class);
 
     @Override
     public void init(FilterConfig config) throws ServletException {
-
+        logger.info("Initialized {}", this.getClass().getName());
     }
 
     @Override
     public void destroy() {
-
+        FenixFramework.shutdown();
     }
 
     @Override
