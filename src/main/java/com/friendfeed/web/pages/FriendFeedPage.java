c053d2c7f54f661c187816c685b78df5b50120f2
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-03-30 02:04:59 +0000

    The location of Static Resources is now configurable on application startup.

--- src/main/java/com/friendfeed/web/pages/FriendFeedPage.java
+++ src/main/java/com/friendfeed/web/pages/FriendFeedPage.java
@@ -1,9 +1,15 @@
 package com.friendfeed.web.pages;
 
+import java.nio.charset.Charset;
+
 import org.apache.wicket.feedback.ErrorLevelFeedbackMessageFilter;
 import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.markup.head.CssUrlReferenceHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptUrlReferenceHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 
+import com.friendfeed.core.util.ConfigurationManager;
 import com.friendfeed.web.FriendFeedApplication;
 import com.friendfeed.web.panel.FriendFeedbackPanel;
 import com.friendfeed.web.panel.TopPanel;
@@ -12,9 +18,23 @@ public abstract class FriendFeedPage extends WebPage {
 
     private static final long serialVersionUID = -2894308070463725312L;
 
+    private static final String staticResourceBase = ConfigurationManager.getProperty("staticResources");
+
     public FriendFeedPage() {
         add(new TopPanel("user", FriendFeedApplication.getCurrentUser()));
         add(new FriendFeedbackPanel("feedbackError", new ErrorLevelFeedbackMessageFilter(FeedbackMessage.WARNING)));
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(new CssUrlReferenceHeaderItem(staticResourceBase + "/css/bootstrap.min.css", "screen", ""));
+        response.render(new CssUrlReferenceHeaderItem(staticResourceBase + "/css/styles.less.css", "screen", ""));
+
+        response.render(new JavaScriptUrlReferenceHeaderItem(staticResourceBase + "/js/jquery.min.js", "", false, Charset
+                .defaultCharset().toString(), ""));
+        response.render(new JavaScriptUrlReferenceHeaderItem(staticResourceBase + "/js/bootstrap.min.js", "", false, Charset
+                .defaultCharset().toString(), ""));
     }
 
 }
