305da0bfa9a0c3b2d07976291d6e24c0a5242193
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-04-16 23:55:43 +0100

    Refactored to comply with the new Atomic API

--- src/main/java/com/friendfeed/web/servlet/CloseTxFilter.java
+++ src/main/java/com/friendfeed/web/servlet/CloseTxFilter.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.Atomic.TxMode;
 import pt.ist.fenixframework.FenixFramework;
 
 @WebFilter("/*")
@@ -32,7 +33,7 @@ public class CloseTxFilter implements Filter {
     }
 
     @Override
-    @Atomic(readOnly = true)
+    @Atomic(mode = TxMode.READ)
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
             ServletException {
         chain.doFilter(request, response);
